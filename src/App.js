5f0eed2f235edb32df73fcb4198d493f3ff2580f
codersachin123 <156166489+codersachin123@users.noreply.github.com>
2024-02-26 14:29:53 +0530

    new component
    self made components with adding css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import VsjItem from './components/Sachin';
 import Sachin from './components/Sachin';
+import Code from './components/Code';
 function App() {
   return (
     <div className="App">
      
-	 <Sachin></Sachin>
+	 <Code></Code>
+   
+   
     </div>
   )
+
 }
 
 export default App;
---
+++ src/components/Code.js
@@ -0,0 +1,11 @@
+import  './Jarvis.css';
+
+function Code()
+{
+	return (<p>
+	
+	<h2 className='Jarvis'> the bes AI</h2>
+	<img src='ironman.jpg'/>
+	</p>);
+}
+export default Code;
