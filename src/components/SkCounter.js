b0ebaa7aabbaf9c947d7904e112a2be3d684e060
codersachin123 <156166489+codersachin123@users.noreply.github.com>
2024-03-02 17:01:06 +0530

    using text field and event begning
    showing 1 change with 7 addition and 1 deletion

--- src/components/SkCounter.js
+++ src/components/SkCounter.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 // import '/SkCounter.css'
-const SkCounter = (props) => {
+const SkCounter = (props) =>
+ {
     const [count, setcount] = useState(0);
-    const [a, b, setadd] = useState(0);
+    const [ num1, setadd1] = useState(0);
+    const [num2, setadd2] = useState(0);
     return <p>
         <h2>Count ={count}</h2>
         <button onClick={() => setcount(count - 1)}>
@@ -12,7 +14,13 @@ const SkCounter = (props) => {
         </button>
         <button onClick={() => setcount(count + 1)}>
             Plu
-        </button>
+</button>
+<input type="Number" onChange={event=> setadd1(event.target.value)}/>
+<input type="Number" onChange={event=> setadd2( event.target.value)}/>
+    <h2>{num1}</h2>
+    <h2>{num2}</h2>
+    <h2>{Number(num1)+Number(num2)}</h2>
+
     </p>
 
 }
