0afc6342bb77c5bf1f5228bef051ae4c19cdab28
codersachin123 <156166489+codersachin123@users.noreply.github.com>
2024-03-23 12:50:46 +0530

    usestate and useeffect
    changing on app js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import TestResult from "./components/TestResult";
+import React, { useState, useEffect } from 'react';
 
 
 function App() {
+  // Declare a state variable called "count" with an initial value of 0
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  });
+
+  const increment = () => {
+    // Update the "count" state by 1
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    // Update the "count" state by -1
+    setCount(count - 1);
+  };
+
   return (
-    <div className="App">
-      
-      <h1>TestResult</h1>
-  <TestResult></TestResult>
-      
-
-
-
-
-
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
-  )
-
+  );
 }
 
 export default App;
