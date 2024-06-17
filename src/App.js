17b9d4da38c13c782412a8849939f992898f5a9e
codersachin123 <156166489+codersachin123@users.noreply.github.com>
2024-06-17 13:21:09 +0530

    transferring of value
    receving the value and transfer the value of num

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import Info from "./components/Info";
+import Subtraction from "./components/subtraction";
 function App() {
     return (
 <div className="App">
-    <h1>Info</h1>
-    <Info></Info>
+    <h1>Subtraction</h1>
+    <Subtraction></Subtraction>
     </div>
     )
 }
