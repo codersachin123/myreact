498162d22af12e6c749cca9b45c8f85d82b03159
codersachin123 <156166489+codersachin123@users.noreply.github.com>
2024-12-20 12:35:26 +0530

    cards_component
    card component with the help of react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react';
-import Carousel from './components/carousel';
+import Card from './components/Card';
 
-function App() {
+
+const App = () => {
+  const cardsData = [
+    {
+      title: 'Card 1',
+      description: 'This is the description for card 1.',
+      image: '/Airpod1.jpg', 
+    },
+    {
+      title: 'Card 2',
+      description: 'This is the description for card 2.',
+      image: '/Airpod3.jpg',
+    },
+    {
+      title: 'Card 3',
+      description: 'This is the description for card 3.',
+      image: '/Apple1.jpg',
+    },
+  ];
+
   return (
-    <div className="App">
-      <h1>My Carousel</h1>
-      <Carousel />
+    <div style={{ display: 'flex', gap: '20px', justifyContent: 'center' }}>
+      {cardsData.map((card, index) => (
+        <Card
+          key={index}
+          title={card.title}
+          description={card.description}
+          image={card.image}
+        />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
+
